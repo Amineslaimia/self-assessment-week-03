55f2f20c54284535901931b63288aaa5717f8dd1
amineslaimia <medamineslaimia@gmail.com>
2019-12-16 10:15:30 +0100

    angular done react bugs

--- react-pond/src/components/FishTable.js
+++ react-pond/src/components/FishTable.js
@@ -1,9 +1,14 @@
-var FishTable = () => (
+var FishTable = (fishes) => (
+
   <table>
     <tbody>
-      <FishTableRow />
-      <FishTableRow />
-      <FishTableRow />
+    {fishes.map(fish => (
+      <FishTableRow
+
+        fish={fish}
+
+      />
+    ))}
     </tbody>
   </table>
 );
